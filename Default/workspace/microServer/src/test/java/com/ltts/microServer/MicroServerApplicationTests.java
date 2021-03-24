75f3490ed33af1ea93b03474ab8f7c520831ba96
Soumyajeet Kundu <78849208+soumyajeet-kundu@users.noreply.github.com>
2021-03-24 17:16:39 +0530

    Add files via upload

---
+++ Default/workspace/microServer/src/test/java/com/ltts/microServer/MicroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltts.microServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
